d65e9629d2a2d7f4497598d94049a06a218f5b9d
Jeff <jbridges1119@gmail.com>
2022-10-03 15:25:54 -0600

    Add create work order api route

--- server/routes/users.js
+++ server/routes/users.js
@@ -23,11 +23,11 @@ module.exports = (db) => {
   });
 
   router.post("/login", (req, res) => {
-    const {company, email, password} = req.body
-    const params = [company, email, password]
+    const { company, email, password } = req.body;
+    const params = [company, email, password];
     const query1 = `SELECT companies.name, email, employees.password, employees.id FROM employees
     JOIN companies ON company_id = companies.id 
-    WHERE company_id = $1 AND email = $2 AND employees.password = $3;`
+    WHERE company_id = $1 AND email = $2 AND employees.password = $3;`;
     const query2 = `SELECT SUM(work_orders.amount) AS total, type, current_bottle_used, bottle_count, first_name, last_name, companies.name  
     FROM employees 
     JOIN work_orders ON employee_id = employees.id 
@@ -39,13 +39,12 @@ module.exports = (db) => {
     db.query(query1, params)
       .then((data1) => {
         const users = data1.rows;
-        if(!users.length) return res.json({ error: "Please enter a valid email and password" });
-        db.query(query2, [users[0].id])
-        .then((data2) => {
+        if (!users.length)
+          return res.json({ error: "Please enter a valid email and password" });
+        db.query(query2, [users[0].id]).then((data2) => {
           const userInfo = data2.rows;
           res.json({ userInfo });
-        })
-        
+        });
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
@@ -53,36 +52,67 @@ module.exports = (db) => {
   });
 
   router.post("/addbottle", (req, res) => {
-    const {company, id, type} = req.body
+    const { company, id, type } = req.body;
     const params = [company, id, type];
     const query1 = `UPDATE refrigerants SET bottle_count = bottle_count + 1, current_bottle_used = 0
     From employee_refrigerant, employees ,companies
     WHERE employees.id = $1 AND companies.id = $2 AND type = $3;`;
     db.query(query1, params)
       .then((data) => {
-        const users = data.rows
+        const users = data.rows;
         res.json({ users });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+//Adds input of refrigerant amount, type, and employee into database under latest work-order number. Then updates employees current bottle amounts used and returns all info for latest work-order 
+  router.post("/workorder", (req, res) => {
+    const { employeeId, type, amount } = req.body;
+    console.log(req.body);
+    const query1 = `SELECT MAX(number) FROM work_orders`;
+    const query2 = `INSERT INTO work_orders (amount, number, date, employee_id)
+    VALUES ($1, $2, CURRENT_DATE, $3) RETURNING id;`;
+    const query3 = `UPDATE refrigerants SET current_bottle_used = current_bottle_used + $2 
+    FROM employee_refrigerant, employees
+    WHERE type = $1 AND employees.id = $3 AND employee_id = employees.id AND refrigerant_id = refrigerants.id
+    RETURNING refrigerants.id;`;
+    const query4 = `INSERT INTO work_order_refrigerant (work_order_id, refrigerant_id)
+    VALUES ($1, $2) RETURNING *;`;
+    const query5 = `SELECT * FROM work_orders 
+    JOIN employees ON employee_id = employees.id
+    JOIN work_order_refrigerant ON work_order_id = work_orders.id
+    JOIN refrigerants ON refrigerant_id = refrigerants.id
+    WHERE work_orders.number = (SELECT MAX(number) FROM work_orders);
+    `;
+    db.query(query1)
+      .then((data1) => {
+        let number = data1.rows[0].max;
+        number++;
+        req.body.map((item) => {
+          const { employeeId, type, amount } = item;
+          const params2 = [amount, number, employeeId];
+          db.query(query2, params2).then((data2) => {
+            const params3 = [type, amount, employeeId];
+            db.query(query3, params3).then((data3) => {
+              const refrigerantId = data3.rows[0].id;
+              const workOrderId = data2.rows[0].id;
+              const params4 = [workOrderId, refrigerantId];
+              db.query(query4, params4);
+            });
+          });
+        });
+        db.query(query5).then((data5) => {
+          const info = data5.rows;
+          res.json({ info });
+        });
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
 
-  router.post("/workorder", (req, res) => {
-    const {company, id, type} = req.body
-    const params = [company, id, type];
-    const query1 = `SELECT MAX(work_orders.id) FROM work_orders`
-    // `INSERT INTO work_orders (amount, date, employee_id)
-    // VALUES (2, CURRENT_DATE, 1);`;
-    db.query(query1)
-      .then((data) => {
-        const users = data.rows
-        res.json({ users });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
+
 
   return router;
 };
@@ -93,7 +123,7 @@ module.exports = (db) => {
 // WHERE employees.id = 1 AND companies.id = 1 AND type = 'R22'`
 
 // `SELECT bottle_count
-//     FROM refrigerants 
+//     FROM refrigerants
 //     JOIN employee_refrigerant ON refrigerant_id = refrigerants.id
 //     JOIN employees ON employee_id = employees.id
 //     JOIN companies ON company_id = companies.id
@@ -101,18 +131,14 @@ module.exports = (db) => {
 
 // const query = `SELECT SUM(amount) AS total FROM employees JOIN work_orders ON employee_id = employees.id WHERE first_name = 'Jeff' AND last_name = 'Bridges' AND number = 1;`;
 
-
-
 //User Overview
 
-
-
 //All Overview with company
-// `SELECT distinct type, current_bottle_used, bottle_count, first_name, last_name, employee_id, SUM(amount) AS total  
-//     FROM employees 
-//     JOIN work_orders ON employee_id = employees.id 
-//     JOIN work_order_refrigerant ON work_order_id = work_orders.id 
-//     JOIN refrigerants ON refrigerant_id = refrigerants.id 
+// `SELECT distinct type, current_bottle_used, bottle_count, first_name, last_name, employee_id, SUM(amount) AS total
+//     FROM employees
+//     JOIN work_orders ON employee_id = employees.id
+//     JOIN work_order_refrigerant ON work_order_id = work_orders.id
+//     JOIN refrigerants ON refrigerant_id = refrigerants.id
 //     JOIN companies ON company_id = companies.id
 //     WHERE company_id = 1
 //     GROUP By refrigerants.type, refrigerants.current_bottle_used,refrigerants.bottle_count, employees.first_name, employees.last_name, work_orders.employee_id
