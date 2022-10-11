fe2620208ba9b457045a76784fd5ab52abe84d1c
Jeff <jbridges1119@gmail.com>
2022-10-11 17:21:00 -0600

    Add simple login for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
-import './App.css';
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./styles/App.css";
 
 function App() {
+  const [email, setEmail] = useState("jbridges1119@gmail.com");
+  const [password, setPassword] = useState("qqqqqqqq");
+  const [loginStatus, setLoginStatus] = useState("");
+  const login = () => {
+    axios
+      .post("http://localhost:3005/users/login", {
+        email: email,
+        password: password,
+      })
+      .then((data) => {
+        if (data.data.error) {
+          setLoginStatus(data.data.error);
+          console.log(data.data.error);
+        } else {
+          setLoginStatus("")
+          console.log(data);
+          
+        }
+      });
+  };
+
   return (
     <div className="App">
+      <Stack>
+      <Typography variant="body1">Email</Typography>
+        <TextField
+          id="email"
+          
+          variant="standard"
+          value="jbridges1119@gmail.com"
+          // onChange={(e) => {
+          //   setEmail(e.target.value);
+          // }}
+        />
+        <Typography variant="body1">Password</Typography>
+        <TextField
+          id="outlined-password-input"
+    
+          autoComplete="current-password"
+          type="password"
+          variant="standard"
+          value="qqqqqqq"
+          // onChange={(e) => {
+          //   setPassword(e.target.value);
+          // }}
+        />
 
+        <Button variant="contained" onClick={login}>
+          Login
+        </Button>
+        <Typography variant="h6">{loginStatus}</Typography>
+      </Stack>
     </div>
   );
 }
