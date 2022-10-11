e8491ceadbc93412dad8592f4d054a3416124126
Jeff <jbridges1119@gmail.com>
2022-10-11 17:20:09 -0600

    Add hashed passwords

--- server/db/seeds/02_employees.sql
+++ server/db/seeds/02_employees.sql
@@ -1,12 +1,12 @@
 INSERT INTO employees (first_name, last_name, email, password, company_id)
-VALUES ('Jeff', 'Bridges', 'jbridges1119@gmail.com', 'q', 1);
+VALUES ('Jeff', 'Bridges', 'jbridges1119@gmail.com', '$2b$10$I0yElg91Q8j3jWKQUcVOQeZILenIXkUhhsK.2SSWGQgdQO47RAjQS', 1);
 INSERT INTO employees (first_name, last_name, email, password, company_id)
-VALUES ('Dave', 'Vanderwal', 'dave@gmail.com', 'q', 1);
+VALUES ('Dave', 'Vanderwal', 'dave@gmail.com', '$2b$10$2tRVq0sWRu2npWzOhu5XEO5suHMQBdZ5PC31ov7VPTbC6lwWXN4Eu', 1);
 INSERT INTO employees (first_name, last_name, email, password, company_id)
-VALUES ('Billy', 'Remple', 'billy@gmail.com', 'q', 1);
+VALUES ('Billy', 'Remple', 'billy@gmail.com', '$2b$10$2tRVq0sWRu2npWzOhu5XEO5suHMQBdZ5PC31ov7VPTbC6lwWXN4Eu', 1);
 INSERT INTO employees (first_name, last_name, email, password, company_id)
-VALUES ('Casey', 'Neels', 'casey@gmail.com', 'q', 1);
+VALUES ('Casey', 'Neels', 'casey@gmail.com', '$2b$10$2tRVq0sWRu2npWzOhu5XEO5suHMQBdZ5PC31ov7VPTbC6lwWXN4Eu', 1);
 INSERT INTO employees (first_name, last_name, email, password, company_id)
-VALUES ('Mark', 'Cameron', 'mark@gmail.com', 'q', 1);
+VALUES ('Mark', 'Cameron', 'mark@gmail.com', '$2b$10$2tRVq0sWRu2npWzOhu5XEO5suHMQBdZ5PC31ov7VPTbC6lwWXN4Eu', 1);
 INSERT INTO employees (first_name, last_name, email, password, company_id)
-VALUES ('Nick', 'Bederosa', 'nick@gmail.com', 'q', 1);
+VALUES ('Nick', 'Bederosa', 'nick@gmail.com', '$2b$10$2tRVq0sWRu2npWzOhu5XEO5suHMQBdZ5PC31ov7VPTbC6lwWXN4Eu', 1);
