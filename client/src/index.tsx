695ce7f9cd1b081b4e06eef55621cb8c9eb8a3e8
Jeff <jbridges1119@gmail.com>
2022-10-11 15:55:56 -0600

    Add axios env option

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import './styles/index.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Provider from "./context/sharedContext";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./hooks/ScrollToTop";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 root.render(
   <React.StrictMode>
     <Provider>
