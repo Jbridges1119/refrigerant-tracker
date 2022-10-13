45d3b651d824893a189e74afb8a988daff7face9
Jeff <jbridges1119@gmail.com>
2022-10-13 17:05:00 -0600

    Change password to hashed password

--- server/db/seeds/01_companies.sql
+++ server/db/seeds/01_companies.sql
@@ -1,4 +1,4 @@
 INSERT INTO companies (name, address, phone, password )
-VALUES ('Cam Air', '313 13th st.', '403-308-2000', 'camair');
+VALUES ('Cam Air', '313 13th st.', '403-308-2000', '$2b$10$2tRVq0sWRu2npWzOhu5XEO5suHMQBdZ5PC31ov7VPTbC6lwWXN4Eu');
 INSERT INTO companies (name, address, phone, password )
-VALUES ('AirWays', 'Another Address', '202-202-0000', 'airways');
+VALUES ('AirWays', 'Another Address', '202-202-0000', '$2b$10$2tRVq0sWRu2npWzOhu5XEO5suHMQBdZ5PC31ov7VPTbC6lwWXN4Eu');
