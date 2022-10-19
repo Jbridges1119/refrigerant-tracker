a0a5e0dee758d6d162323b97c7d110b924f6e796
Jeff <jbridges1119@gmail.com>
2022-10-19 15:02:44 -0600

    Add login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
             <Route path="/" element={<LandingPage />} />
           </>
         ) : (
-          <Route path="/*" element={<Login />} />
+          <Route path="/login" element={<Login />} />
         )}
       </Routes>
       <Footer />
